cf3524cdc96e95fb59e6852841ddb73d789616ef
fis007 <schaffhauserfilip@gmail.com>
2020-06-27 15:23:00 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <TourList />
+      </React.Fragment>
+    )
+
+  }
+}
+
+export default App;
